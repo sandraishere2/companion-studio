b3eb03d0a578559a307a0b4158dda900f4e1a03c
sandraishere2 <sandraishere2@gmail.com>
2026-07-30 15:59:17 +1000

    Create Exportfuntion.js
    import jwt from "jsonwebtoken";
    
    export function authenticateToken(req, res, next) {
      const authHeader = req.headers["authorization"];
      const token = authHeader && authHeader.split(" ")[1];
      if (!token) {
        return res.status(401).json({ error: "Login required" });
      }
      try {
        const decoded = jwt.verify(token, process.env.JWT_SECRET);
        req.user = { id: decoded.userId };
        next();
      } catch {
        return res.status(403).json({ 
          error: "Invalid or expired token" 
        });
      }
    }

---
+++ Exportfuntion.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Login required" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { id: decoded.userId };
+    next();
+  } catch {
+    return res.status(403).json({ 
+      error: "Invalid or expired token" 
+    });
+  }
+}
